75cba93556c90bdf65c4b069fe543f231220b7b7
islam <islamkappusev64@gmail.com>
2023-03-01 14:59:19 +0300

    Перевел доску в ООП

--- graphics.py
+++ graphics.py
@@ -1,40 +1,38 @@
 import pygame as pg
 
-pg.init()
 
-RES = WEDTH, HEIGHT = 800, 800
+class Board:
+    pg.init()
 
-sc = pg.display.set_mode(RES)
-pg.display.set_caption('Chess')
-icon = pg.image.load('pics/big floppa.jpg')
-pg.display.set_icon(icon)
-floppa = pg.transform.scale(icon, (800, 800))
-floppa.set_alpha(100)
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-BLEDZOLOT = (238, 232, 170)
-OHRA = (160, 82, 45)
+    RES = WEDTH, HEIGHT = 800, 800
 
-FPS = 60
+    sc = pg.display.set_mode(RES)
+    pg.display.set_caption('Chess')
+    icon = pg.image.load('pics/big floppa.jpg')
+    pg.display.set_icon(icon)
+    floppa = pg.transform.scale(icon, (800, 800))
+    floppa.set_alpha(100)
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    BLEDZOLOT = (238, 232, 170)
+    OHRA = (160, 82, 45)
+    FPS = 60
+    size = 100
 
-size = 100
-y = 0
+    def draw_board(self):
+        clock = pg.time.Clock()
+        while True:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    pg.quit()
+                    quit()
 
-clock = pg.time.Clock()
-
-while True:
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            pg.quit()
-            quit()
-
-    count = 0
-    for x in range(8):
-        for y in range(8):
-            if (x + y) % 2 == 0:
-                pg.draw.rect(sc, BLEDZOLOT, [size * x, size * y, size, size])
-            else:
-                pg.draw.rect(sc, OHRA, [size * x, size * y, size, size])
-    sc.blit(floppa, (0, 0))
-    pg.display.update()
-    clock.tick(FPS)
+            for x in range(8):
+                for y in range(8):
+                    if (x + y) % 2 == 0:
+                        pg.draw.rect(self.sc, self.BLEDZOLOT, [self.size * x, self.size * y, self.size, self.size])
+                    else:
+                        pg.draw.rect(self.sc, self.OHRA, [self.size * x, self.size * y, self.size, self.size])
+            self.sc.blit(self.floppa, (0, 0))
+            pg.display.update()
+            clock.tick(self.FPS)
